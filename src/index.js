6cd4263e05330d6e9c904603a0ed5ce409d682c9
rusty-reebs <rusty_reebs@tutanota.com>
2021-08-19 11:46:24 +0400

    import renderHome function from home.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Main JS script to call other functions
+
+//! Separate DOM stuff from application logic, keep in separate modules
+
+import { renderHome } from "./components/home";
+
+renderHome();
+
+
+
