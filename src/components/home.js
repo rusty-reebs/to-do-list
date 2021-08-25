cf33988b33d1647d45a59b1d0c18598c2a597d06
rusty-reebs <rusty_reebs@tutanota.com>
2021-08-25 08:48:53 +0400

    added export saveNote and import saveNote, and set attribute onclick

--- src/components/home.js
+++ src/components/home.js
@@ -2,8 +2,7 @@
 
 //! Separate DOM stuff from application logic, keep in separate modules
 
-// const main = document.createElement("div"); //? how to keep these in the module pattern and then return them? DONE
-// const notesContainer = document.createElement("div");
+import { saveNote } from "../index.js"; //! not working, says it is never read
 
 const renderHome = (() => {
   const content = document.getElementById("content");
@@ -91,15 +90,11 @@ const notePopup = () => {
 
   const noteForm = document.createElement("form");
   noteForm.classList.add("noteform");
+  noteForm.id = "noteform";
   popupBox.appendChild(noteForm);
 
   const formBreak = document.createElement("br");
   const div = document.createElement("div");
-
-  const labelDueDate = document.createElement("label");
-  labelDueDate.classList.add("duedate");
-  labelDueDate.for = "duedate";
-  labelDueDate.innerHTML = "Due Date:";
 
   const inputTitle = document.createElement("input");
   inputTitle.classList.add("notetitle");
@@ -115,6 +110,11 @@ const notePopup = () => {
   inputDescrip.id = "notedescription";
   inputDescrip.placeholder = "Take a note...";
 
+  const labelDueDate = document.createElement("label");
+  labelDueDate.classList.add("duedate");
+  labelDueDate.for = "duedate";
+  labelDueDate.innerHTML = "Due Date:";
+
   const inputDueDate = document.createElement("input");
   inputDueDate.classList.add("duedatecal");
   inputDueDate.type = "date";
@@ -128,6 +128,7 @@ const notePopup = () => {
 
   const selectPriority = document.createElement("select");
   selectPriority.name = "priority";
+  selectPriority.id = "priority";
   const highPri = document.createElement("option");
   highPri.value = "high";
   highPri.innerHTML = "High";
@@ -137,15 +138,13 @@ const notePopup = () => {
   const lowPri = document.createElement("option");
   lowPri.value = "low";
   lowPri.innerHTML = "Low";
-  selectPriority.appendChild(highPri);
-  selectPriority.appendChild(medPri);
-  selectPriority.appendChild(lowPri);
 
-  const noteSubmitButton = document.createElement("button"); // doesn't like this
+  const noteSubmitButton = document.createElement("button");
   noteSubmitButton.type = "submit";
   noteSubmitButton.classList.add("savebutton");
   noteSubmitButton.name = "submit";
   noteSubmitButton.id = "submitnote";
+  noteSubmitButton.setAttribute("onclick", "saveNote();"); //! not working
   noteSubmitButton.innerHTML = "Save";
 
   noteForm.appendChild(inputTitle);
@@ -155,19 +154,15 @@ const notePopup = () => {
   noteForm.appendChild(inputDueDate);
   noteForm.appendChild(labelPriority);
   noteForm.appendChild(selectPriority);
+  selectPriority.appendChild(highPri);
+  selectPriority.appendChild(medPri);
+  selectPriority.appendChild(lowPri);
   noteForm.appendChild(noteSubmitButton);
 
-  // noteForm.appendChild(inputPriorityHigh);
-  // noteForm.appendChild(labelPriorityHigh);
-  // noteForm.appendChild(inputPriorityMed);
-  // noteForm.appendChild(labelPriorityMed);
-  // noteForm.appendChild(inputPriorityLow);
-  // noteForm.appendChild(labelPriorityLow);
+  return { noteForm };
 };
 
 export { notePopup };
-
-// TODO finish styling note popup and add save button
 
 // const labelPriorityHigh = document.createElement("label");
 // labelPriorityHigh.for = "priorityhigh";
@@ -198,3 +193,10 @@ export { notePopup };
 // inputPriorityLow.name = "priority";
 // inputPriorityLow.id = "prioritylow";
 // inputPriorityLow.value = "Low";
+
+// noteForm.appendChild(inputPriorityHigh);
+// noteForm.appendChild(labelPriorityHigh);
+// noteForm.appendChild(inputPriorityMed);
+// noteForm.appendChild(labelPriorityMed);
+// noteForm.appendChild(inputPriorityLow);
+// noteForm.appendChild(labelPriorityLow);
--- src/index.js
+++ src/index.js
@@ -3,5 +3,63 @@
 //! Separate DOM stuff from application logic, keep in separate modules
 
 import { renderHome } from "./components/home";
+import { notePopup } from "./components/home";
 
-renderHome();
+renderHome;
+notePopup();
+
+// TODO build note object
+// TODO build note array
+// TODO render array in notes
+
+class Note {
+  constructor(title, description, duedate, priority) {
+    this.title = title;
+    this.description = description;
+    this.duedate = duedate;
+    this.priority = priority;
+  }
+}
+
+// note array module
+
+const noteArray = (() => {
+  const notes = [];
+  return { notes };
+})();
+
+// module for creating a note
+
+const form = document.getElementById("noteform");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("click");
+});
+
+const noteTitle = document.getElementById("notetitle");
+const noteDescription = document.getElementById("notedescription");
+const noteDueDate = document.getElementById("duedate");
+const notePriority = document.getElementById("priority");
+
+const addtoStorage = () => {
+  localStorage.setItem("notes", JSON.stringify(noteArray.notes));
+};
+
+const getfromStorage = () => {
+  noteArray.notes = JSON.parse(localStorage.getItem("notes"));
+};
+
+const saveNote = () => {
+  todoNote = new Note(
+    noteTitle.value,
+    noteDescription.value,
+    noteDueDate.value,
+    notePriority.value
+  );
+  noteArray.notes.push(todoNote);
+  console.log(noteArray.notes);
+};
+
+console.log(noteArray.notes);
+
+export { saveNote };
