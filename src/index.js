514bdebdbf1eaf19bd4077d72fcf6f98f3cd70c8
rusty-reebs <rusty_reebs@tutanota.com>
2021-09-01 11:46:44 +0400

    added highlights to priority text and trash icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 //! Separate DOM stuff from application logic, keep in separate modules
 
-//? Look at `data-id` attribute to grab notes with event listeners. Look at the game jam.
+//? Look at `data-string` attribute to grab notes with event listeners. Look at the game jam.
 
 import { renderHome } from "./components/home";
 import { notePopup } from "./components/home";
@@ -27,7 +27,14 @@ renderHome.newNoteButton.addEventListener("click", () => {
   notePopup();
 });
 
-// TODO fix event listener. When I click a note, I get multiple listeners firing
+noteForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  saveNote();
+  console.log("saved!");
+});
+
+// TODO event listeners good, except for +New Note when popup box is open
+// if (e.target !== btn) btn.classList.remove("active"); EXAMPLE
 
 window.addEventListener("click", ({ target }) => {
   if (
@@ -38,10 +45,17 @@ window.addEventListener("click", ({ target }) => {
     console.log("clicked off the box");
     popupContainer.classList.toggle("show");
     closeNotePopup();
+  } else if (
+    !popupContainer.classList.contains("show") &&
+    target.closest(".note")
+  ) {
+    console.log("you clicked a note");
+    console.log(target.closest(".note"));
+    editNote(); //? pass in some useful note id
   }
 });
 
-//! also think about a newnote module to separate things
+//! also think about a newnote module to separate things. Read the objectives again.
 
 const saveNote = () => {
   const noteTitle = document.getElementById("notetitle");
@@ -57,6 +71,7 @@ const saveNote = () => {
   );
   noteArray.notes.push(todoNote);
   console.log(noteArray.notes);
+  //? need to set a unique identifier for a note? To be used for editing? Data-id
   populateBoard();
   setTimeout(() => closeNotePopup(), 300);
 };
@@ -75,36 +90,70 @@ const populateBoard = () => {
   clearBoard();
   noteArray.notes.forEach((note, index) => {
     const blankNote = document.createElement("div");
+    //? set data-id here?
     blankNote.classList.add("note");
     const title = document.createElement("div");
     title.classList.add("notedivtitle");
+    title.classList.add("scroll");
     title.textContent = note.title;
     blankNote.appendChild(title);
     const description = document.createElement("div");
     description.classList.add("notediv");
+    description.classList.add("scroll");
     description.textContent = note.description;
     blankNote.appendChild(description);
     const duedate = document.createElement("div");
-    duedate.classList.add("notediv");
-    duedate.textContent = note.duedate;
+    duedate.classList.add("notedivduedate");
+    // duedate.textContent = "Due Date: " + note.duedate;
     blankNote.appendChild(duedate);
+    const duedateLabel = document.createElement("div");
+    duedateLabel.textContent = "Due Date:";
+    const duedateDate = document.createElement("div");
+    duedateDate.textContent = note.duedate;
+    // duedate.appendChild(duedateLabel);
+    duedate.appendChild(duedateDate);
     const priority = document.createElement("div");
-    priority.classList.add("notediv");
-    priority.textContent = note.priority;
+    priority.classList.add("notedivpriority");
+    // priority.textContent = "Priority: " + note.priority;
     blankNote.appendChild(priority);
+    const priorityLabel = document.createElement("div");
+    priorityLabel.textContent = "Priority:";
+    const priorityValue = document.createElement("div");
+    priorityValue.textContent = note.priority;
+    switch (note.priority) {
+      case "High":
+        priorityValue.classList.add("high");
+        break;
+      case "Medium":
+        priorityValue.classList.add("medium");
+        break;
+      case "Low":
+        priorityValue.classList.add("low");
+        break;
+      default:
+        console.log("no priority selected");
+    }
+    // priority.appendChild(priorityLabel);
+    priority.appendChild(priorityValue);
+    const trashIconDiv = document.createElement("div");
+    trashIconDiv.classList.add("trashicondiv");
+    const trashIcon = document.createElement("i");
+    trashIcon.classList.add("fa");
+    trashIcon.classList.add("fa-trash");
+    trashIconDiv.appendChild(trashIcon);
+    priority.appendChild(trashIconDiv);
     renderHome.notesContainer.appendChild(blankNote);
     addToStorage();
-    blankNote.addEventListener("click", () => {
-      console.log("You clicked a note");
-      console.log(note);
-      editNote();
-      // TODO when you click a note, it should open the popup and fill in the existing values
-    });
+    //? may still need blankNote.forEach eventlistener to get actual note
   });
 };
 
 const editNote = () => {
+  //? can pass in a note id parameter?
   notePopup();
+  // TODO fill in existing values ⬇️ THIS
+  // note popup box comes up for a specific note. Need to get the data for that note and put it in the data fields.
+  // Need to save the edit by removing the old object and inserting the new object. May need different event listener?
 };
 
 const clearBoard = () => {
@@ -112,12 +161,6 @@ const clearBoard = () => {
     renderHome.notesContainer.removeChild(renderHome.notesContainer.firstChild);
   }
 };
-
-noteForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  saveNote();
-  console.log("saved!");
-});
 
 const addToStorage = () => {
   localStorage.setItem("notes", JSON.stringify(noteArray.notes));
